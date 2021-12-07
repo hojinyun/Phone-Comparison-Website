65a33ad2a8fb20487ae2d106424cc05ce3ee7b9b
Hojin Yun <hojinyun.dev@gmail.com>
2021-12-07 23:15:41 +0900

    created skeleton of the website

--- Phone_Scrapper/main.py
+++ Phone_Scrapper/main.py
@@ -1,4 +1,5 @@
 from scrapper import extract_phones
 from export import save_to_file
+from flask import Flask
 
-save_to_file(extract_phones(1)) #10 pages
+save_to_file(extract_phones(3)) #10 pages
--- Website/main.py
+++ Website/main.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template, request, redirect,send_file
 import requests
+import csv
+import pandas as pd
 
-app = Flask(__name__)
-
+app = Flask("Comparison Website")
 
 @app.route('/')
 def home():
-    return "Hello Flask World!"
+  term = request.form.get("term")
+  return render_template("home.html",term=term)
+
+
+@app.route('/search')
+def search():
+  term=request.args.get('term')
+  df = pd.read_csv('phones.csv', encoding='euc-kr')
+  found_phones = pd.DataFrame(columns=df.columns)
+  #found_phones.columns = df.columns
+  print(found_phones)
+  #for row in phone_result:
+   # if term in row[0]:
+    #  db.app
+
+
+ # return render_template("search.html", term=term, )
+
+  return render_template("search_result.html")
+
+@app.route('/compare')
+def compare():
+    return "Compare"
+
+app.run(host="0.0.0.0")
