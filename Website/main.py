9ceda5b580964cad8e88b4101a7eeb620d36c172
Hojin Yun <hojinyun.dev@gmail.com>
2021-12-08 20:21:11 +0900

    added search function

--- Website/main.py
+++ Website/main.py
@@ -15,20 +15,33 @@ def home():
 def search():
   term=request.args.get('term')
   df = pd.read_csv('phones.csv', encoding='euc-kr')
+  df = df.dropna()
   found_phones = pd.DataFrame(columns=df.columns)
-  #found_phones.columns = df.columns
-  print(found_phones)
-  #for row in phone_result:
-   # if term in row[0]:
-    #  db.app
+  for row in df.iterrows():
+    if term in row[1]['기종']:
+      #print(row[1])
+      #print("--------------------------------------")
+      found_phones=found_phones.append(row[1])
+  #print(found_phones.iloc[0])
+  for phone in found_phones.iterrows():
+    print(phone[1]['기종'])
+
+  return render_template("search_result.html", results=found_phones.iterrows(), term=term)
+  #print(df)
+    #if term in row[0]:
+      
+      #found_phones.add(row)
+    #  print(row)
+  print(found_phones.index.values)
 
 
  # return render_template("search.html", term=term, )
 
-  return render_template("search_result.html")
+  #return render_template("search_result.html")
 
 @app.route('/compare')
 def compare():
+      
     return "Compare"
 
 app.run(host="0.0.0.0")
